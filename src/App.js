4f73b4aa5d887fa6650dc7773bb84519ac404f78
N Mahadevan <81569232+mahadevan028@users.noreply.github.com>
2021-06-07 14:06:50 +0530

    added src files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Homepage from './components/Homepage-component/Homepage.jsx';
+
+function App() {
+  return (  
+    <div>
+      <Homepage />
+     <h1> Hiiiiiiiii</h1>
+    </div>
+  );
+}
+
+export default App;
