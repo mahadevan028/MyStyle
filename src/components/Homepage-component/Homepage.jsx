765a8cf077d51ed1a00e2c850afb8eeb53121d8d
N Mahadevan <81569232+mahadevan028@users.noreply.github.com>
2021-06-07 14:02:49 +0530

    commit-component

---
+++ src/components/Homepage-component/Homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+const homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <div className="title">
+                            <div className="sub-title">
+                             <h1>Hats</h1>
+                             <span>Shop Now</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default homepage;
